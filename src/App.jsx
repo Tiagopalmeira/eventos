52a82a5bbde35be800aff5f56f67d035f5e90fb6
Tiago Santana Palmeira <125421932+Tiagopalmeira@users.noreply.github.com>
2025-01-30 16:54:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     // Fetch events from API
     axios
-      .get("${apiUrl}")
+      .get(`${apiUrl}/eventos`)
       .then((response) => setEvents(response.data))
       .catch((error) => console.error("Erro ao buscar eventos:", error));
   }, []);
