e41982aa50ed17399f0999646fd70df98bbff374
Tiago Santana Palmeira <125421932+Tiagopalmeira@users.noreply.github.com>
2025-01-30 17:39:27 -0300

    Update EventCard.jsx

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,6 +1,21 @@
 import React, { useState } from "react";
 import "../styles.css";
 
+// Função para formatar a data corretamente
+const formatDate = (dateString) => {
+  // Ajustando a data para UTC com o horário de meio-dia, para evitar problemas de fuso horário
+  const date = new Date(dateString);
+  const utcDate = new Date(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    12,
+    0,
+    0
+  ); // Forçando hora para 12:00:00 UTC
+  return utcDate.toLocaleDateString("pt-BR"); // Formatando no formato brasileiro
+};
+
 function EventAccordion({ event }) {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -8,21 +23,19 @@ function EventAccordion({ event }) {
     <div className="event-accordion">
       <div className="event-summary" onClick={() => setIsOpen(!isOpen)}>
         <p>
-          {new Date(event.startDate).toLocaleDateString()} - {event.title}
+          {formatDate(event.startDate)} - {event.title}
         </p>
       </div>
       {isOpen && (
         <div className="event-details">
           <p>
-            <strong>Data de Início:</strong>{" "}
-            {new Date(event.startDate).toLocaleDateString()}
+            <strong>Data de Início:</strong> {formatDate(event.startDate)}
           </p>
           <p>
             <strong>Hora de Início:</strong> {event.startTime}
           </p>
           <p>
-            <strong>Data de Término:</strong>{" "}
-            {new Date(event.endDate).toLocaleDateString()}
+            <strong>Data de Término:</strong> {formatDate(event.endDate)}
           </p>
           <p>
             <strong>Hora de Término:</strong> {event.endTime}
